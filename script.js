62004447a200d2a577d725422e769372309dad98
ulugbekbrat <168886327+ulugbekbrat@users.noreply.github.com>
2024-08-20 10:17:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+ let ism = prompt ('ismingizni kiriting')
+
+let harf = prompt ('bironta harf kiriting')
+
+if(ism>harf){
+    
+alert(`sizning ismingizda ${harf} harfi mavjud 😎`)
+
+}else{
+
+    alert(`sizning ismingizda ${harf} harfi mavjud emas 🤷‍♂️  `)
+
+}
